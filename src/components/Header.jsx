34c6d07a718116257c97c8588e58468a25740d89
Han Htoo Satt <hsatt@una.edu>
2024-10-08 01:26:55 -0500

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ const Header = ({ title, description, total }) => {
   return (
     <div className="header">
       <h1>{title}</h1>
-      <p>{description}</p>
-      <p>Total Cards: {total}</p>
+      <h3>{description}</h3>
+      <h3>Total Cards: {total}</h3>
     </div>
   );
 };
--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,32 +1,48 @@
 const cardData = [
     {
-      question: "What is the capital of France?",
-      answer: "Paris",
-      image: "https://via.placeholder.com/150", 
+      question: "Who was the first man to walk on the moon?",
+      answer: "Neil Armstrong",
       color: "#FFD700"
     },
     {
       question: "What is the tallest mountain in the world?",
       answer: "Mount Everest",
-      image: "https://via.placeholder.com/150",
       color: "#FF6347"
     },
     {
-      question: "What is 1 + 1?",
-      answer: "2",
+      question: "In which year did the Titanic sink?",
+      answer: "1912",
       color: "#87CEEB"
     },
     {
-      question: "What is the capital of Myanmar(Burma)?",
-      answer: "Naypyidaw",
-      image: "", 
-      color: "#FFD700"
+      question: "What is the tallest building in the world?",
+      answer: "Burj Khalifa",
+      color: "#FF6347"
     },
     {
-      question: "What is the tallest building in the world?",
-      answer: "Mount Everest",
-      image: "https://via.placeholder.com/150",
-      color: "#FF6347"
+      question: "Who was the first President of the United States?",
+      answer: "George Washington",
+      color: "#6B8E23"
+    },
+    {
+      question: "How many bones are in the human body?",
+      answer: "206",
+      color: "#FF33A1"
+    },
+    {
+      question: "Which animal is known as the ‘Ship of the Desert’?",
+      answer: "Camel",
+      color: "#FF5733"
+    },
+    {
+      question: "Which is the largest ocean on Earth?",
+      answer: "Pacific Ocean",
+      color: "#3357FF"
+    },
+    {
+      question: "What is the longest river in the world?",
+      answer: "Nile River",
+      color: "#33FFF5"
     },
     {
       question: "What is 1 + 1 in Discrete Math?",
